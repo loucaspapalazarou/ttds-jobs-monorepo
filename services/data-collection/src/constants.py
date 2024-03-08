5603c114d4173b1e45c4541c226b10bc8128d674
Loukas Papalazarou <loukas.papalazarou.cy@gmail.com>
2024-03-08 10:56:39 +0000

    bug fix

--- services/data-collection/src/constants.py
+++ services/data-collection/src/constants.py
@@ -6,7 +6,7 @@ load_dotenv()
 CONSTANTS = {
     "postgres_host": os.getenv("PG_HOSTNAME"),
     "postgres_user": os.getenv("PG_USERNAME"),
-    "postgres_password": os.getenv("POSTGRES_PASSWORD"),
+    "postgres_password": os.getenv("PG_PASSWORD"),
     "postgres_db": os.getenv("PG_DB_NAME"),
     "scrape_duration": 14_400,  # 14,400 seconds = 4 hours
     "scrape_time": "00:00",
