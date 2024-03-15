53286b27e4a41c8342306e7da5928eeff6255e1a
Loukas Papalazarou <loukas.papalazarou.cy@gmail.com>
2024-03-15 18:12:52 +0000

    fixed feedback error

--- services/backend/src/services/feedbackService.py
+++ services/backend/src/services/feedbackService.py
@@ -1,12 +1,20 @@
 from fastapi import Request
-
 from ..models.feedbackModel import FeedbackData
 
 
 async def save_feedback_to_database(feedback_data: FeedbackData, request: Request):
-    insert_statement = (f"INSERT INTO feedback (rating, feedback, email, date) "
-                        f"VALUES ({feedback_data.rating}, {feedback_data.feedback}, "
-                        f"{feedback_data.email}, {feedback_data.date});")
+    insert_statement = (
+        "INSERT INTO feedback (rating, feedback, email, date) "
+        "VALUES (%s, %s, %s, %s)"
+    )
 
-    await request.app.state.db.fetch_rows(insert_statement)
+    # Replace None with empty string in the data tuple
+    data_tuple = (
+        feedback_data.rating if feedback_data.rating is not None else "",
+        feedback_data.feedback if feedback_data.feedback is not None else "",
+        feedback_data.email if feedback_data.email is not None else "",
+        feedback_data.date if feedback_data.date is not None else "",
+    )
+
+    await request.app.state.db.execute(insert_statement, *data_tuple)
     return
