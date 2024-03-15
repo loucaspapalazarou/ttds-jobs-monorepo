4b42514e5469e927086e4912f4c618e8fa94be2d
Loukas Papalazarou <loukas.papalazarou.cy@gmail.com>
2024-03-15 19:05:34 +0000

    removed email from everywhere

--- services/backend/src/services/feedbackService.py
+++ services/backend/src/services/feedbackService.py
@@ -4,14 +4,11 @@ from ..models.feedbackModel import FeedbackData
 
 
 async def save_feedback_to_database(feedback_data: FeedbackData, request: Request):
-    insert_statement = (
-        "INSERT INTO feedback (rating, feedback, email) " "VALUES ($1, $2, $3)"
-    )
+    insert_statement = "INSERT INTO feedback (rating, feedback) " "VALUES ($1, $2)"
 
     data_tuple = (
         feedback_data.rating,
         feedback_data.feedback,
-        feedback_data.email,
     )
 
     await request.app.state.db.insert_feedback(insert_statement, data_tuple)
